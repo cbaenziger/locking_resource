2082929aeaa04cad9e5339de183af6a1d933e277
Clay Baenziger <cwb@clayb.net>
2017-02-15 13:18:52 -0500

    Rubocop-ify code

--- libraries/locking_resource.rb
+++ libraries/locking_resource.rb
@@ -21,7 +21,8 @@ class Chef
     attribute(:perform, kind_of: Symbol, required: true)
     attribute(:timeout, kind_of: Integer, default: \
       lazy { node['locking_resource']['restart_lock_acquire']['timeout'] })
-    attribute(:zookeeper_hosts, kind_of: Array, default: lazy { node['locking_resource']['zookeeper_servers'] })
+    attribute(:zookeeper_hosts, kind_of: Array, default: \
+      lazy { node['locking_resource']['zookeeper_servers'] })
     attribute(:process_pattern, option_collector: true)
     attribute(:lock_data, kind_of: String, default: lazy { node['fqdn'] })
 
@@ -41,39 +42,49 @@ class Chef
         # to avoid namespace collisions replace spaces in resource name with
         # a colon -- zookeeper's quite permissive on paths:
         # https://zookeeper.apache.org/doc/trunk/zookeeperProgrammers.html#ch_zkDataModel
-        lock_name = new_resource.lock_name or new_resource.name.gsub(' ', ':')
+        lock_name = new_resource.lock_name || new_resource.name.tr_s(' ', ':')
         lock_path = ::File.join(
           node['locking_resource']['restart_lock']['root'],
-          lock_name)
+          lock_name
+        )
 
         zk_hosts = parse_zk_hosts(new_resource.zookeeper_hosts)
-        unless node['locking_resource']['skip_restart_coordination']
+        if node['locking_resource']['skip_restart_coordination']
+          got_lock = false
+          Chef::Log.warn 'Restart coordination disabled -- skipping lock ' \
+                         "acquisition on #{lock_path}"
+        else
           Chef::Log.info "Acquiring lock #{lock_path}"
           # acquire lock
+          # rubocop:disable no-and-or-or
           got_lock = lock_matches?(zk_hosts, lock_path, new_resource.lock_data)\
-            and Chef::Log.info "Found stale lock"
+            and Chef::Log.info 'Found stale lock'
+          # rubocop:enable no-and-or-or
+
           # intentionally do not use a timeout to avoid leaving a wonky
           # zookeeper object or connection if we interrupt it -- thus we trust
           # the zookeeper object to not wantonly hang
           start_time = Time.now
           while !got_lock && (start_time + new_resource.timeout) >= Time.now
+            # rubocop:disable no-and-or-or
             got_lock = create_node(zk_hosts, lock_path, new_resource.lock_data)\
               and Chef::Log.info 'Acquired new lock'
-            sleep(node['locking_resource']['restart_lock_acquire']['sleep_time'])
+            # rubocop:enable no-and-or-or
+            sleep(
+              node['locking_resource']['restart_lock_acquire']['sleep_time']
+            )
           end
           # see if we ever got a lock -- if not record it for later
-          if !got_lock
-            need_rerun(node,lock_path)
-          end
-        else
-          got_lock = false
-          Chef::Log.warn 'Restart coordination disabled -- skipping lock ' \
-                         "acquisition on #{lock_path}"
+          need_rerun(node, lock_path) unless got_lock
         end
 
         # affect the resource, if we got the lock -- or error
-        if got_lock or node['locking_resource']['skip_restart_coordination']
+        if got_lock || node['locking_resource']['skip_restart_coordination']
           notifying_block do
+            unless r
+              raise "Unable to find resource #{new_resource.resource} in " \
+                'resources'
+            end
             r.run_action new_resource.perform
             r.resolve_notification_references
             new_resource.updated_by_last_action(r.updated)
@@ -84,7 +95,7 @@ class Chef
             end
           end
         else
-          raise 'Failed to acquire lock for ' +
+          raise 'Failed to acquire lock for ' \
                 "LockingResource[#{new_resource.name}], path #{lock_path}"
         end
       end
@@ -94,12 +105,13 @@ class Chef
     # and the service has not restarted since we got the lock
     def action_serialize_process
       vppo = ::LockingResource::Helper::VALID_PROCESS_PATTERN_OPTS
-      raise "Need a process pattern attribute" unless \
-        new_resource.process_pattern.length != 0
-      raise "Only expect options: #{vppo.keys} but got " \
-        "#{new_resource.process_pattern.keys}" if \
-        Set.new(new_resource.process_pattern.keys) < \
+      raise 'Need a process pattern attribute' unless \
+        new_resource.process_pattern.!empty?
+      if Set.new(new_resource.process_pattern.keys) < \
         Set.new(vppo.keys)
+        raise "Only expect options: #{vppo.keys} but got " \
+          "#{new_resource.process_pattern.keys}"
+      end
       converge_by("serializing as process #{new_resource.name} via lock") do
         l_time = false
         lock_and_rerun = false
@@ -108,12 +120,13 @@ class Chef
         zk_hosts = parse_zk_hosts(new_resource.zookeeper_hosts)
 
         # convert keys from strings to symbols for process_start_time()
-        start_time_arg = new_resource.process_pattern.inject({}) do |memo,(k,v)|
+        start_time_arg = \
+            new_resource.process_pattern.each_with_object({}) do |memo, (k, v)|
           memo[k.to_sym] = v
           memo
         end
 
-        p_start = process_start_time(start_time_arg) or false
+        p_start = process_start_time(start_time_arg) || false
 
         # if the process is not running we do not care about lock management --
         # just run the action
@@ -122,23 +135,27 @@ class Chef
           # the lock as we may have multiple resources with the same name
           lock_path = ::File.join(
             node['locking_resource']['restart_lock']['root'],
-            new_resource.name.gsub(' ', ':'))
+            new_resource.name.tr_s(' ', ':')
+          )
 
+          # rubocop:disable no-and-or-or
           got_lock = lock_matches?(zk_hosts, lock_path, new_resource.lock_data)\
             or return
+          # rubocop:enable no-and-or-or
+
           l_time = get_node_ctime(zk_hosts, lock_path)
-          Chef::Log.warn "Found stale lock" if got_lock
+          Chef::Log.warn 'Found stale lock' if got_lock
         end
 
         r_time = rerun_time?(node, lock_path)
         # verify we are holding the lock and need to re-run
-        lock_and_rerun = (p_start <= (r_time or Time.new(0)) and l_time) \
+        lock_and_rerun = (p_start <= (r_time || Time.new(0)) && l_time) \
           if p_start
 
-        if !p_start or \
-           lock_and_rerun or \
-           p_start <= (l_time or Time.new(1970))
-          Chef::Log.warn "Restarting process: lock time " \
+        if !p_start || \
+           lock_and_rerun || \
+           p_start <= (l_time || Time.new(1970))
+          Chef::Log.warn 'Restarting process: lock time ' \
                          "#{l_time}; rerun state #{r_time}; " \
                          "process restarted #{p_start}"
           notifying_block do
@@ -158,4 +175,3 @@ class Chef
     end
   end
 end
-
